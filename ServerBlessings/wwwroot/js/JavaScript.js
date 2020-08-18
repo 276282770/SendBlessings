66afa1880da8ef469b5d854d46f77de3d8bf0966
Codu <276282770@qq.com>
2020-08-18 08:45:09 +0800

    save

--- ServerBlessings/wwwroot/js/JavaScript.js
+++ ServerBlessings/wwwroot/js/JavaScript.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(
+    
     function () {
-       
+
         setTimeout(
             
             function () {
@@ -177,4 +178,10 @@ function send() {
             console.log(ret);
         }
     });
+}
+//获取url中的参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
 }
